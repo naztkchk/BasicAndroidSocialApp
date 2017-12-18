9f0b8a7094f2a832d1f79d5e58b3c91058991434
draxvel <ntv3331998@gmail.com>
2017-12-18 12:19:55 +0200

    Implemented 6a tass (created data models for Albums and Image)

---
+++ app/src/main/java/com/pllug/course/tkachuk/basicandroidsocialapp/model/Album.java
@@ -0,0 +1,38 @@
+package com.pllug.course.tkachuk.basicandroidsocialapp.model;
+
+public class Album {
+
+    private int userId;
+    private int id;
+    private String title;
+
+    public Album(int userId, int id, String title) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ app/src/main/java/com/pllug/course/tkachuk/basicandroidsocialapp/model/Photo.java
@@ -0,0 +1,58 @@
+package com.pllug.course.tkachuk.basicandroidsocialapp.model;
+
+public class Photo {
+
+    private int albumId;
+    private int id;
+    private String title;
+    private String url;
+    private String thumbnailUrl;
+
+    public Photo(int albumId, int id, String title, String url, String thumbnailUrl) {
+        this.albumId = albumId;
+        this.id = id;
+        this.title = title;
+        this.url = url;
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public int getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(int albumId) {
+        this.albumId = albumId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
